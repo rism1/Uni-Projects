2f1a062868762975369e810012c39b0989a2afae
danya <35501515+rism1@users.noreply.github.com>
2022-05-05 10:29:37 +0300

    Update biginteger.h

--- biginteger.h
+++ biginteger.h
@@ -3,6 +3,15 @@
 #include <string>
 #include <iostream>
 
+typedef signed char        int8_t;
+typedef short              int16_t;
+typedef int                int32_t;
+typedef long long          int64_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
+
 /*
 Friend functions could be outside the class without the keyword but I think it is more neat to place it within the class
 Also, I couldn't clear all lnt-arithmetic-overflow warnings because of essential casts
